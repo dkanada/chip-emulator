a7de3dff71c8ad755f39f398d9804736e6718c4c
dkanada <dkanada@users.noreply.github.com>
2018-01-08 16:48:43 -0700

    add two new classes for opcodes and registers

---
+++ app/src/main/java/com/dkanada/chip/core/OPCode.java
@@ -0,0 +1,29 @@
+package com.dkanada.chip.core;
+
+public class OPCode {
+    private char opcode;
+
+    public OPCode(char value) {
+        opcode = value;
+    }
+
+    public char getNNN() {
+        return (char) (opcode & 0x0FFF);
+    }
+
+    public char getNN() {
+        return (char) (opcode & 0x00FF);
+    }
+
+    public char getN() {
+        return (char) (opcode & 0x000F);
+    }
+
+    public char getX() {
+        return (char) ((opcode & 0x0F00) >> 8);
+    }
+
+    public char getY() {
+        return (char) ((opcode & 0x00F0) >> 4);
+    }
+}
