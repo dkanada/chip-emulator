0e7c154144eb62ef7200c094ffc3d4203847ebba
dkanada <dkanada@users.noreply.github.com>
2018-01-11 18:30:47 -0700

    restart app after closing the settings activity

--- app/src/main/java/com/dkanada/chip/activities/SettingsActivity.java
+++ app/src/main/java/com/dkanada/chip/activities/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.dkanada.chip.activities;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -45,5 +46,13 @@ public class SettingsActivity extends AppCompatActivity {
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         getWindow().setNavigationBarColor(getResources().getColor(R.color.primary));
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        overridePendingTransition(0, 0);
+        startActivity(intent);
     }
 }
